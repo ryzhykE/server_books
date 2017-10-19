d39e57b5d113fbd6d3d9dc40ce220cde7092726d
stoyat <evgenii_r84@mail.ru>
2017-10-19 18:24:12 +0300

    19101

--- book_shop.sql
+++ book_shop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Окт 12 2017 г., 17:44
+-- Время создания: Окт 19 2017 г., 18:22
 -- Версия сервера: 5.7.11
 -- Версия PHP: 7.0.4
 
@@ -17,34 +17,58 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `book_shop`
+-- База данных: `books_shop`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `author`
+-- Структура таблицы `authors`
 --
 
-CREATE TABLE IF NOT EXISTS `author` (
+CREATE TABLE IF NOT EXISTS `authors` (
   `id` int(11) NOT NULL,
-  `name_author` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `authors`
+--
+
+INSERT INTO `authors` (`id`, `name`) VALUES
+(1, 'Johanna Basford'),
+(2, 'Rupi Kaur'),
+(3, 'Paul Auster'),
+(4, 'B. A. Paris'),
+(5, 'Roald Dahl'),
+(6, 'George R. R. Martin'),
+(7, 'J.K. Rowling'),
+(8, 'Jane Austen');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `book`
+-- Структура таблицы `books`
 --
 
-CREATE TABLE IF NOT EXISTS `book` (
+CREATE TABLE IF NOT EXISTS `books` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `discription` text NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `img` varchar(255) NOT NULL DEFAULT 'no_image.jpg',
-  `discount` float NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `price` decimal(7,2) NOT NULL,
+  `description` text NOT NULL,
+  `discount` decimal(7,2) DEFAULT '0.00',
+  `active` enum('yes','no') NOT NULL DEFAULT 'yes',
+  `img` varchar(255) NOT NULL DEFAULT 'no_image.jpg'
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `price`, `description`, `discount`, `active`, `img`) VALUES
+(1, 'Ivy and the Inky Butterfly', '12.95', 'Bring your own colour to the story. From colouring book queen Johanna Basford, a lavishly illustrated fable about a girl named Ivy who stumbles upon a secret door leading to the magical world of Enchantia. A charming story that interacts playfully with beautiful, colourable artwork in Johanna''s signature style, Ivy and the Inky Butterfly is a one-of-a-kind adventure for the whole family and readers of all ages to customise, colour and cherish. Johanna has picked a crisp ivory paper that accentuates and compliments your chosen colour palette. The smooth, untextured pages allows for beautiful blending or gradient techniques with coloured pencils, or are perfect for pens, allowing the nib to glide evenly over the surface without feathering.', '5.00', 'yes', 'static/img/butterfly.jpg'),
+(3, '4 3 2 1', '21.77', 'LONGLISTED FOR THE MAN BOOKER PRIZE 2017On March 3, 1947, in the maternity ward of Beth Israel Hospital in Newark, New Jersey, Archibald Isaac Ferguson, the one and only child of Rose and Stanley Ferguson, is born. From that single beginning, Ferguson''s life will take four simultaneous and independent fictional paths. Four Fergusons made of the same genetic material, four boys who are the same boy, will go on to lead four parallel and entirely different lives. Family fortunes diverge. Loves and friendships and intellectual passions contrast. Chapter by chapter, the rotating narratives evolve into an elaborate dance of inner worlds enfolded within the outer forces of history as, one by one, the intimate plot of each Ferguson''s story rushes on across the tumultuous and fractured terrain of mid twentieth-century America. A boy grows up-again and again and again.As inventive and dexterously constructed as anything Paul Auster has ever written 4 3 2 1 is an unforgettable tour de force, the crowning work of this masterful writer''s extraordinary career.', '0.00', 'yes', 'static/img/4321.jpg'),
+(4, '\r\nThe Breakdown: The 2017 gripping thriller from the bestselling author of Behind Closed Doors', '21.21', '`A psychological page-turner'' - Good Housekeeping If you can''t trust yourself, who can you trust? It all started that night in the woods. Cass Anderson didn''t stop to help the woman in the car, and now she''s dead. Ever since, silent calls have been plaguing Cass and she''s sure someone is watching her. Consumed by guilt, she''s also starting to forget things. Whether she took her pills, what her house alarm code is - and if the knife in the kitchen really had blood on it. Bestselling author B A Paris is back with a brand new psychological thriller full of twists and turns that will keep you on the edge of your seat.', '0.00', 'yes', 'static/img/break.jpg');
 
 -- --------------------------------------------------------
 
@@ -57,16 +81,40 @@ CREATE TABLE IF NOT EXISTS `book_to_author` (
   `id_author` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `book_to_author`
+--
+
+INSERT INTO `book_to_author` (`id_book`, `id_author`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(3, 3),
+(4, 4);
+
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `book_to_ganre`
+-- Структура таблицы `book_to_genre`
 --
 
-CREATE TABLE IF NOT EXISTS `book_to_ganre` (
+CREATE TABLE IF NOT EXISTS `book_to_genre` (
   `id_book` int(11) NOT NULL,
   `id_genre` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `book_to_genre`
+--
+
+INSERT INTO `book_to_genre` (`id_book`, `id_genre`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(3, 3),
+(4, 4),
+(1, 2),
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -79,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `cart` (
   `id_book` int(11) NOT NULL,
   `id_client` int(11) NOT NULL,
   `count` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -89,14 +137,24 @@ CREATE TABLE IF NOT EXISTS `cart` (
 
 CREATE TABLE IF NOT EXISTS `client` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `login` varchar(255) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `role` enum('admin','user') NOT NULL,
-  `discount` float NOT NULL DEFAULT '0',
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `login` varchar(11) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `discount` decimal(7,0) DEFAULT '0',
+  `hash` varchar(255) DEFAULT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
   `active` enum('yes','no') NOT NULL DEFAULT 'yes'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `client`
+--
+
+INSERT INTO `client` (`id`, `first_name`, `last_name`, `login`, `pass`, `discount`, `hash`, `role`, `active`) VALUES
+(10, 'Evgeniy', 'Ryzhik', 'evgeniy', '1b2b1c1b9a4f61971e9332ba3772cdd8', NULL, NULL, 'user', 'yes'),
+(11, 'Evgeniy', 'Ryzhik', 'login', 'd9b1d7db4cd6e70935368a1efb10e377', '11', 'c9c7e253873d2ad3db79b6813bed0957', 'user', 'yes'),
+(12, 'admin__n', 'admin_l', 'admin', 'ec6a6536ca304edf844d1d248a4f08dc', '0', '8e1c048dcf01be3d720748be68a8f947', 'admin', 'yes');
 
 -- --------------------------------------------------------
 
@@ -106,8 +164,23 @@ CREATE TABLE IF NOT EXISTS `client` (
 
 CREATE TABLE IF NOT EXISTS `genre` (
   `id` int(11) NOT NULL,
-  `name_genre` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `genre`
+--
+
+INSERT INTO `genre` (`id`, `name`) VALUES
+(1, 'detective'),
+(2, 'thriller'),
+(3, 'drama'),
+(4, 'documental literature'),
+(5, 'art'),
+(6, 'love story'),
+(7, 'poetry'),
+(8, 'novel'),
+(9, 'western');
 
 -- --------------------------------------------------------
 
@@ -118,8 +191,9 @@ CREATE TABLE IF NOT EXISTS `genre` (
 CREATE TABLE IF NOT EXISTS `history_book` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `genre` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
-  `genre` varchar(255) NOT NULL
+  `price` decimal(7,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -130,38 +204,53 @@ CREATE TABLE IF NOT EXISTS `history_book` (
 
 CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(11) NOT NULL,
-  `id_book` int(11) NOT NULL,
   `id_client` int(11) NOT NULL,
-  `id_status` int(11) NOT NULL,
-  `time` date NOT NULL,
+  `total_discount` decimal(7,0) DEFAULT NULL,
+  `status` enum('processed','sent') NOT NULL DEFAULT 'processed',
   `id_payment` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `date_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `total_price` decimal(7,0) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=98 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `id_client`, `total_discount`, `status`, `id_payment`, `date_time`, `total_price`) VALUES
+(94, 11, '25', 'processed', 1, '2017-10-19 09:05:33', '332'),
+(95, 11, '1', 'processed', 1, '2017-10-19 09:27:19', '20'),
+(96, 11, '28', 'processed', 1, '2017-10-19 11:38:15', '227'),
+(97, 11, '82', 'processed', 1, '2017-10-19 11:40:29', '634');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `order_full_info`
+-- Структура таблицы `orders_full_info`
 --
 
-CREATE TABLE IF NOT EXISTS `order_full_info` (
+CREATE TABLE IF NOT EXISTS `orders_full_info` (
   `id` int(11) NOT NULL,
+  `id_order` int(11) NOT NULL,
   `id_book` int(11) NOT NULL,
   `title_book` varchar(255) NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `discount` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+  `count` int(11) NOT NULL,
+  `price` decimal(7,0) NOT NULL,
+  `discount_book` decimal(7,0) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
--- Структура таблицы `order_status`
+-- Дамп данных таблицы `orders_full_info`
 --
 
-CREATE TABLE IF NOT EXISTS `order_status` (
-  `id` int(11) NOT NULL,
-  `name_statut` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `orders_full_info` (`id`, `id_order`, `id_book`, `title_book`, `count`, `price`, `discount_book`) VALUES
+(1, 94, 4, '\r\nThe Breakdown: The 2017 gripping thriller from the bestselling author of Behind Closed Doors', 6, '21', '0'),
+(2, 94, 3, '4 3 2 1', 4, '22', '0'),
+(3, 94, 1, 'Ivy and the Inky Butterfly', 11, '13', '5'),
+(4, 95, 4, '\r\nThe Breakdown: The 2017 gripping thriller from the bestselling author of Behind Closed Doors', 1, '21', '0'),
+(5, 96, 4, '\r\nThe Breakdown: The 2017 gripping thriller from the bestselling author of Behind Closed Doors', 12, '21', '0'),
+(6, 97, 3, '4 3 2 1', 25, '22', '0'),
+(7, 97, 1, 'Ivy and the Inky Butterfly', 5, '13', '5'),
+(8, 97, 4, '\r\nThe Breakdown: The 2017 gripping thriller from the bestselling author of Behind Closed Doors', 5, '21', '0');
 
 -- --------------------------------------------------------
 
@@ -170,39 +259,47 @@ CREATE TABLE IF NOT EXISTS `order_status` (
 --
 
 CREATE TABLE IF NOT EXISTS `payment` (
-  `id` int(5) NOT NULL,
-  `name_pay` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(11) NOT NULL,
+  `name` varchar(15) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `payment`
+--
+
+INSERT INTO `payment` (`id`, `name`) VALUES
+(1, 'Visa'),
+(2, 'MasterCart');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `author`
+-- Индексы таблицы `authors`
 --
-ALTER TABLE `author`
+ALTER TABLE `authors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `book`
+-- Индексы таблицы `books`
 --
-ALTER TABLE `book`
+ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `book_to_author`
 --
 ALTER TABLE `book_to_author`
-  ADD KEY `book_to_author_fk0` (`id_book`),
-  ADD KEY `book_to_author_fk1` (`id_author`);
+  ADD KEY `book_to_author_fk0` (`id_author`),
+  ADD KEY `book_to_author_fk1` (`id_book`);
 
 --
--- Индексы таблицы `book_to_ganre`
+-- Индексы таблицы `book_to_genre`
 --
-ALTER TABLE `book_to_ganre`
-  ADD KEY `book_to_ganre_fk0` (`id_book`),
-  ADD KEY `book_to_ganre_fk1` (`id_genre`);
+ALTER TABLE `book_to_genre`
+  ADD KEY `book_to_genre_fk0` (`id_genre`),
+  ADD KEY `book_to_genre_fk1` (`id_book`);
 
 --
 -- Индексы таблицы `cart`
@@ -217,7 +314,7 @@ ALTER TABLE `cart`
 --
 ALTER TABLE `client`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `login` (`login`);
+  ADD KEY `login` (`login`);
 
 --
 -- Индексы таблицы `genre`
@@ -236,22 +333,16 @@ ALTER TABLE `history_book`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `orders_fk0` (`id_book`),
-  ADD KEY `orders_fk1` (`id_client`),
-  ADD KEY `orders_fk2` (`id_status`),
-  ADD KEY `orders_fk3` (`id_payment`);
+  ADD KEY `orders_fk0` (`id_client`),
+  ADD KEY `orders_fk1` (`id_payment`);
 
 --
--- Индексы таблицы `order_full_info`
+-- Индексы таблицы `orders_full_info`
 --
-ALTER TABLE `order_full_info`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `order_status`
---
-ALTER TABLE `order_status`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `orders_full_info`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orders_full_info_fk0` (`id_order`),
+  ADD KEY `orders_full_info_fk1` (`id_book`);
 
 --
 -- Индексы таблицы `payment`
@@ -264,30 +355,30 @@ ALTER TABLE `payment`
 --
 
 --
--- AUTO_INCREMENT для таблицы `author`
+-- AUTO_INCREMENT для таблицы `authors`
 --
-ALTER TABLE `author`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
--- AUTO_INCREMENT для таблицы `book`
+-- AUTO_INCREMENT для таблицы `books`
 --
-ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT для таблицы `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=54;
 --
 -- AUTO_INCREMENT для таблицы `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT для таблицы `genre`
 --
 ALTER TABLE `genre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT для таблицы `history_book`
 --
@@ -297,22 +388,17 @@ ALTER TABLE `history_book`
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=98;
 --
--- AUTO_INCREMENT для таблицы `order_full_info`
+-- AUTO_INCREMENT для таблицы `orders_full_info`
 --
-ALTER TABLE `order_full_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `order_status`
---
-ALTER TABLE `order_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orders_full_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT для таблицы `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
@@ -321,31 +407,36 @@ ALTER TABLE `payment`
 -- Ограничения внешнего ключа таблицы `book_to_author`
 --
 ALTER TABLE `book_to_author`
-  ADD CONSTRAINT `book_to_author_fk0` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
-  ADD CONSTRAINT `book_to_author_fk1` FOREIGN KEY (`id_author`) REFERENCES `author` (`id`);
+  ADD CONSTRAINT `book_to_author_fk0` FOREIGN KEY (`id_author`) REFERENCES `authors` (`id`),
+  ADD CONSTRAINT `book_to_author_fk1` FOREIGN KEY (`id_book`) REFERENCES `books` (`id`);
 
 --
--- Ограничения внешнего ключа таблицы `book_to_ganre`
+-- Ограничения внешнего ключа таблицы `book_to_genre`
 --
-ALTER TABLE `book_to_ganre`
-  ADD CONSTRAINT `book_to_ganre_fk0` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
-  ADD CONSTRAINT `book_to_ganre_fk1` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id`);
+ALTER TABLE `book_to_genre`
+  ADD CONSTRAINT `book_to_genre_fk0` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id`),
+  ADD CONSTRAINT `book_to_genre_fk1` FOREIGN KEY (`id_book`) REFERENCES `books` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `cart`
 --
 ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_fk0` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
+  ADD CONSTRAINT `cart_fk0` FOREIGN KEY (`id_book`) REFERENCES `books` (`id`),
   ADD CONSTRAINT `cart_fk1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
-  ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`),
-  ADD CONSTRAINT `orders_fk2` FOREIGN KEY (`id_status`) REFERENCES `order_status` (`id`),
-  ADD CONSTRAINT `orders_fk3` FOREIGN KEY (`id_payment`) REFERENCES `payment` (`id`);
+  ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`),
+  ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`id_payment`) REFERENCES `payment` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `orders_full_info`
+--
+ALTER TABLE `orders_full_info`
+  ADD CONSTRAINT `orders_full_info_fk0` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `orders_full_info_fk1` FOREIGN KEY (`id_book`) REFERENCES `books` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
